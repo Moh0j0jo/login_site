979bf5c0fafe975604f18beaeaaae09502af54be
Moh0j0jo <peter.pinnel@gmail.com>
2023-10-05 16:01:02 +0200

    site version 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import './App.css';
 
 function App() {
   
+ 
   return (
     <div className="App">
+      <h1>Welcome back</h1>
       <div className="login_wrapper">
-        <img src="#" alt="" />
-        <p>Welcome back</p>
-        <p>User Name</p>
-        <form class="login_form" action="">
-          <input type="email" name="email" id="" />
-          <input type="password" name="psswrd" id="" />
-          <button>LOGIN</button>
+        <form className="login_form" action="">
+          <input type="email" name="email" id="login_email"
+            placeholder='Provide email' />
+          <input type="password" name="psswrd" placeholder='Provide password' id="login_psswrd" />
+          <div className="button-wrapper">
+            <button>Login</button>
+            <button>Sign up</button>
+          </div>
+
         </form>
       </div>
+      <p>Forgot your password?</p>
     </div>
   );
 
