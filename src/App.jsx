0ab539b4c44ef17cba8ec6d8ecfe3d0e8581217c
Moh0j0jo <peter.pinnel@gmail.com>
2023-10-05 14:47:36 +0200

    base

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <div className="login_wrapper">
+        <img src="#" alt="" />
+        <p>Welcome back</p>
+        <p>User Name</p>
+        <form class="login_form" action="">
+          <input type="email" name="email" id="" />
+          <input type="password" name="psswrd" id="" />
+          <button>LOGIN</button>
+        </form>
+      </div>
+    </div>
+  );
+
+}
+
+export default App;
